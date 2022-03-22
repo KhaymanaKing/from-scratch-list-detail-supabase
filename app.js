b5e27da72e3493642e6e741c0144d24acfad4ff4
Khayman King <khaymanak@gmail.com>
2022-03-22 14:57:17 -0700

    app.js async func is done for displayall

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// import functions and grab DOM elements
+// import functions and grab DOM elements\
+import {getDnDClass } from './fetch-utils.js';
 
 // let state
 
@@ -6,3 +7,20 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+const listEl = document.querySelector();
+
+window.addEventListener('load', async () => {
+    fetchAndDisplayClasses();
+});
+
+async function fetchAndDisplayClasses(){
+
+    const dndClasses = await getDnDClass();
+    for (let dndClass of dndClasses) {
+
+        const dndClassEl = renderClasses(dndClass);
+        listEl.append(dndClassEl);
+}
+}
